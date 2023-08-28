9c3810ff27edeca58a94fa09b0b2f18cd1160dbd
Maísa Moreira <maisamoreira@Maisas-MacBook-Air.local>
2023-08-28 14:40:37 -0300

    primeiro commit

---
+++ src/main/java/com/pigrafos/Main.java
@@ -0,0 +1,10 @@
+package com.pigrafos;
+
+public class Main {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+    }
+}
